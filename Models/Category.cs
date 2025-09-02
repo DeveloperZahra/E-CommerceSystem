5feedcc30f0b9611ef167e8ecb0a3a12ea8c174b
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-09-02 15:29:17 +0400

    Marks CategoryId as the Primary Key for the Category table

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,10 @@
-﻿namespace E_CommerceSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceSystem.Models
 {
     public class Category
     {
+        [Key] 
+        public int CategoryId { get; set; }  // Marks CategoryId as the Primary Key for the Category table
     }
 }
