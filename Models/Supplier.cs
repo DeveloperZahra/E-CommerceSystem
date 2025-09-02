c8d3e8a4fbf1ec56dd6ce91cc10bb100c256b49c
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-09-02 15:33:26 +0400

    [Key] → Marked SupplierId as the Primary Key for the Supplier entity

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -1,6 +1,12 @@
-﻿namespace E_CommerceSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceSystem.Models
 {
     public class Supplier
     {
+        // [Key] → Marked SupplierId as the Primary Key for the Supplier entity
+
+        [Key]
+        public int SupplierId { get; set; }
     }
 }
